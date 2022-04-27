9f0c3f8ce7d3f0fd2081a3e99077fc092a0c1bdc
simen raes <75736482+simenraes@users.noreply.github.com>
2022-04-27 14:12:11 +0200

    apointments show in h2 console + added data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into Afspraak (ID, DATUM, START_UUR, EIND_UUR, NAAM)
+values ((nextval('AFSPRAAK_SEQ')), '2021-09-3', '16:00', '17:00', 'test');
+insert into Afspraak (ID, DATUM, START_UUR, EIND_UUR, NAAM)
+values ((nextval('AFSPRAAK_SEQ')), '2021-09-3', '16:00', '17:00', 'test');
+insert into Afspraak (ID, DATUM, START_UUR, EIND_UUR, NAAM)
+values ((nextval('AFSPRAAK_SEQ')), '2021-09-3', '16:00', '17:00', 'test');
+insert into Afspraak (ID, DATUM, START_UUR, EIND_UUR, NAAM)
+values ((nextval('AFSPRAAK_SEQ')), '2021-09-3', '16:00', '17:00', 'test');
+insert into Afspraak (ID, DATUM, START_UUR, EIND_UUR, NAAM)
+values ((nextval('AFSPRAAK_SEQ')), '2021-09-3', '16:00', '17:00', 'test');
+insert into Afspraak (ID, DATUM, START_UUR, EIND_UUR, NAAM)
+values ((nextval('AFSPRAAK_SEQ')), '2021-09-3', '16:00', '17:00', 'test');
