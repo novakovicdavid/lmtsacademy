ab645ae46a2ef5c06ec62376f7ff49516a251c08
simen raes <75736482+simenraes@users.noreply.github.com>
2022-05-03 15:31:12 +0200

    added data into event table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,3 +17,9 @@ insert into USER (ID, USERNAME, PASSWORD, ROLE) values (nextval('USER_SEQ'), 'ad
 INSERT INTO COURSE (ID,NAME,TEACHER,DESCRIPTION,SHORTDESCRIPTION) VALUES(1,'cursus1','leekracht1','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum','Lorem ipsum dolor sit amet, consectetur adipiscing elit');
 INSERT INTO COURSE (ID,NAME,TEACHER,DESCRIPTION,SHORTDESCRIPTION) VALUES(2,'cursus2','leekracht2','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum','dolor sit amet, consectetur adipiscing elit');
 
+
+insert into event(id, title, start, finish, description) values (1, 'event1', '2015-01-01 1:00:00', '2015-01-01 2:00:00', 'description1');
+insert into event(id, title, start, finish, description) values (2, 'event2', '2015-01-02 2:00:00', '2015-01-02 3:00:00', 'description1');
+insert into event(id, title, start, finish, description) values (3, 'event3', '2015-01-03 1:00:00', '2015-01-03 2:00:00', 'description1');
+insert into event(id, title, start, finish, description) values (4, 'event4', '2015-01-04 1:00:00', '2015-01-04 2:00:00', 'description1');
+
