3d21864218c367840a21f6bf68a6b53c7e1f353e
Arnolep <arnolepee@gmail.com>
2022-05-10 15:52:14 +0200

    Prev_Next_Buttons_Coursedetails

--- src/main/java/com/example/demo/repositories/CourseRepository.java
+++ src/main/java/com/example/demo/repositories/CourseRepository.java
@@ -3,6 +3,15 @@ package com.example.demo.repositories;
 import com.example.demo.model.Course;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CourseRepository extends CrudRepository<Course,Integer> {
 
+    Optional<Course> findFirstByIdLessThanOrderByIdDesc(int id);
+
+    Optional<Course> findFirstByOrderByIdDesc();
+
+    Optional<Course> findFirstByIdGreaterThanOrderByIdAsc(int id);
+
+    Optional<Course> findFirstByOrderByIdAsc();
 }
