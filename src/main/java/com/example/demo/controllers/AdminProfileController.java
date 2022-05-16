9faf5b8f02a8989aae1a7c32dad26a995887546d
David <83370778+novakovicdavid@users.noreply.github.com>
2022-05-16 16:13:40 +0200

    Sort profiles list by id descending

--- src/main/java/com/example/demo/controllers/AdminProfileController.java
+++ src/main/java/com/example/demo/controllers/AdminProfileController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ public class AdminProfileController {
     @GetMapping({"/profiles/{page}"})
     public String getMembersAtPage(@PathVariable Integer page,
                              Model model) {
-        var profiles = profileRepository.findAll(PageRequest.of(page, 20));
+        var profiles = profileRepository.findAll(PageRequest.of(page, 20, Sort.by("id").descending()));
         model.addAttribute("profiles", profiles.iterator());
         return "admin/profiles";
     }
