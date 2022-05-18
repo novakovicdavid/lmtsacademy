ea5f4117bf254b16ed2457e2a434a039cc14b45a
David <83370778+novakovicdavid@users.noreply.github.com>
2022-05-18 02:38:23 +0200

    Admin profiles page initial styling

--- src/main/java/com/example/demo/controllers/AdminProfileController.java
+++ src/main/java/com/example/demo/controllers/AdminProfileController.java
@@ -21,7 +21,7 @@ public class AdminProfileController {
     @GetMapping({"/profiles/{page}"})
     public String getMembersAtPage(@PathVariable Integer page,
                              Model model) {
-        var profiles = profileRepository.findAll(PageRequest.of(page, 10, Sort.by("id").descending()));
+        var profiles = profileRepository.findAll(PageRequest.of(page, 25, Sort.by("id").descending()));
         model.addAttribute("profiles", profiles.iterator());
         model.addAttribute("page", page);
         model.addAttribute("nextPageHasContent", (profiles.getTotalPages() - (page + 1)) != 0);
@@ -37,7 +37,7 @@ public class AdminProfileController {
     public String getMembersAtPageFilter(@PathVariable Integer page,
                                          ProfilesFilter filter,
                                          Model model) {
-        var profiles = profileRepository.findAllByFilter(PageRequest.of(page, 10, Sort.by("id").descending()), filter.getFirstName(), filter.getLastName(), filter.getEmail(), filter.getIsNew() != null);
+        var profiles = profileRepository.findAllByFilter(PageRequest.of(page, 25, Sort.by("id").descending()), filter.getFirstName(), filter.getLastName(), filter.getEmail(), filter.getIsNew() != null);
         model.addAttribute("profiles", profiles);
         model.addAttribute("page", page);
         model.addAttribute("nextPageHasContent", (profiles.getTotalPages() - (page + 1)) != 0);
