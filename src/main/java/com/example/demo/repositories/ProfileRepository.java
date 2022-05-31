0ff6b7443d18f9b61b3d206b2d083e0804c7a49e
David <83370778+novakovicdavid@users.noreply.github.com>
2022-05-31 17:13:46 +0200

    Fix profiles page performance

--- src/main/java/com/example/demo/repositories/ProfileRepository.java
+++ src/main/java/com/example/demo/repositories/ProfileRepository.java
@@ -7,11 +7,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ProfileRepository extends PagingAndSortingRepository<Profile, Integer> {
-    @Query(value = "select profile from Profile profile where " +
+    @Query(value = "select profile from Profile profile " +
+            "join fetch profile.user user where " +
             "(:firstName is null or UPPER(profile.firstName) like CONCAT('%',UPPER(CAST(:firstName as text)),'%')) and " +
             "(:lastName is null or UPPER(profile.lastName) like CONCAT('%',UPPER(CAST(:lastName as text)),'%')) and " +
             "(:showNewOnly = false or (:showNewOnly = true and :showNewOnly = profile.isNew)) and " +
-            "(:email is null or UPPER(profile.user.email) like CONCAT('%',UPPER(CAST(:email as text)),'%')) and" +
-            "((:hideDisabled = true and profile.user.enabled = true) or :hideDisabled = false)")
+            "(:email is null or UPPER(user.email) like CONCAT('%',UPPER(CAST(:email as text)),'%')) and " +
+            "((:hideDisabled = true and user.enabled = true) or :hideDisabled = false)",
+    countQuery = "select count(profile) from Profile profile where " +
+            "(:firstName is null or UPPER(profile.firstName) like CONCAT('%',UPPER(CAST(:firstName as text)),'%')) and " +
+            "(:lastName is null or UPPER(profile.lastName) like CONCAT('%',UPPER(CAST(:lastName as text)),'%')) and " +
+            "(:showNewOnly = false or (:showNewOnly = true and :showNewOnly = profile.isNew)) and " +
+            "(:email is null or UPPER(user.email) like CONCAT('%',UPPER(CAST(:email as text)),'%')) and " +
+            "((:hideDisabled = true and user.enabled = true) or :hideDisabled = false)")
     Page<Profile> findAllByFilter(Pageable page, String firstName, String lastName, String email, Boolean showNewOnly, Boolean hideDisabled);
 }
